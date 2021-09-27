5b774df3b8e4c18d09363cbad0692522033f234f
abeno-br <abeno@abeno.org.br>
2021-09-27 10:18:02 -0300

    Primeiro commit do projeto

---
+++ dist/precache-manifest.a128fdf9705ca58d9bb1eb02b1429e6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeec364a7eb79114e9d5",
+    "url": "/css/chunk-vendors.984d8db7.css"
+  },
+  {
+    "revision": "4f5577ab892829ee95068ade59fa9ca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c65b83c817d831960014",
+    "url": "/js/app.6f91850a.js"
+  },
+  {
+    "revision": "ebb1e17a0bf333dcb74d",
+    "url": "/js/chunk-2d21d18e.cd5b45c4.js"
+  },
+  {
+    "revision": "aeec364a7eb79114e9d5",
+    "url": "/js/chunk-vendors.d85a5010.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
